28a81bb60e79c8d1c26f8f2ec2aed05e9c284fa6
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-07-05 15:15:01 +0530

    added 404 Page

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Contact from './Pages/Contact';
+import Notfound from './Pages/Notfound';
 
 const App = () => {
   return (
-    <Router basename='pages'>
+    <Router>
       <div className="App">
         <Header />
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/about' component={About} />
           <Route path='/contact' component={Contact} />
+          <Route component={Notfound} />
         </Switch>
       </div>
     </Router>
