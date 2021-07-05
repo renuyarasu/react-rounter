3d43d9e45efa6103c0157fa9c97228d855267293
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-07-05 15:02:17 +0530

    added basename

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
 import Header from './components/Header';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Contact from './Pages/Contact';
 
 const App = () => {
   return (
-    <Router>
+    <Router basename='pages'>
       <div className="App">
         <Header />
-      <Route path='/' exact component={Home} />
-      <Route path='/about' component={About} />
-      <Route path='/contact' component={Contact} />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+        </Switch>
       </div>
     </Router>
   );
